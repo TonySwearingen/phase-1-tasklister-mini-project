7cfe4477cc9c9290899c6fd3092e94d4cb656d94
unlimited1974@aol.com <unlimited1974@aol.com>
2022-01-18 16:12:46 -0600

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) =>{
+      e.preventDefault()
+      buildToDo(e.target.newTaskDescription.value)
+      form.reset()
+  })
+})
+
+function buildToDo(todo) {
+    let p = document.createElement('p')
+    
+
+    let btn2 = document.createElement('button') 
+    btn2.addEventListener('click', handleEdit)
+    btn2.textContent = 'edit'
+    btn2.style.background = "#green"
+    btn2.style.marginLeft = '2px'
+    
+
+    let btn = document.createElement('button')
+    btn.addEventListener('click', handleDelete)
+    btn.textContent = 'x'
+    p.textContent = `${todo}  `
+    p.append(btn, btn2)
+    document.querySelector('#tasks').appendChild(p)
+}
+
+function handleDelete(e) {
+e.target.parentNode.remove()
+}
+
+function handleEdit(e) {
+    console.log(e.target.parentNode)
+    
+//     e.target.parentNode.innerHTML += `
+//     <form id="edit-form" action="" method="POST">
+//     <label for="new-task-description">Task description:</label>
+//     <input type="text" id="newTaskDescription" name="new-task-description" placeholder="description">
+//     <input type="submit" value="Edit Task">
+//   </form>
+//     `
+
+}
